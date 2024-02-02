26c751862f4020f24205b2debfaf29db597bd899
Жуков Иван <152768804+Zhoochok@users.noreply.github.com>
2024-02-02 16:19:05 +0300

    createTable

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,11 @@
-const fs = require("fs");
-const { EOL } = require("os");
+/* eslint-disable consistent-return */
+const { table } = require('console');
+const fs = require('fs');
+const { EOL } = require('os');
 
-const puzzlePath = "./puzzles.txt";
+const puzzlePath = './puzzles.txt';
 function read() {
-  const puzzle = fs.readFileSync(puzzlePath, "utf-8");
+  const puzzle = fs.readFileSync(puzzlePath, 'utf-8');
   return puzzle.trim().split(EOL);
 }
 // console.log(read());
@@ -19,43 +21,44 @@ function random() {
 }
 
 function solve(text) {
-  const res = text.match(/.{1,9}/gi).map((el) => el.split(""));
+  const res = text.match(/.{1,9}/gi).map((el) => el.split(''));
 
+  // eslint-disable-next-line no-restricted-syntax
   for (arr of res) {
     for (let i = 0; i < arr.length; i += 1) {
-      if (arr[i] === "-") {
+      if (arr[i] === '-') {
         arr[i] = random().toString();
       }
     }
   }
   return res;
 }
-console.table(solve(text));
+// console.log(solve(text));
 
-const sudo = solve(text)
+const sudo = solve(text);
 // console.log(sudo);
 
+// Принимает игровое поле в том формате, в котором его вернули из функции solve.
+// Возвращает булевое значение — решено это игровое поле или нет.
+// eslint-disable-next-line no-shadow
+
 function isSolved(sudo) {
-  for(el of sudo){
-    for(let i = 0; i < el.length; i += 1){
-      if(/[1-9]*/g){
-        return false
-      }
-      
+  // eslint-disable-next-line no-restricted-syntax
+  for (el of sudo) {
+    for (let i = 0; i < el.length; i += 1) {
+      if (/[1-9]*/g) {
+        return false;
       }
     }
   }
+}
 
 console.log(isSolved(sudo));
+
 // Принимает игровое поле в том формате, в котором его вернули из функции solve.
-  // Возвращает булевое значение — решено это игровое поле или нет.
-
-
-
-
-
+// Выводит в консоль/терминал судоку.
+// Подумай, как симпатичнее его вывести.
 function prettyBoard() {
-  // Принимает игровое поле в том формате, в котором его вернули из функции solve.
-  // Выводит в консоль/терминал судоку.
-  // Подумай, как симпатичнее его вывести.
+  console.table(solve(text));
 }
+prettyBoard();
